1fcf0b6d9be385f3fbcababa24fb546c2f39d189
Alex Gu <sualexx@aol.com>
2019-05-07 04:00:15 +0300

    implement /v1/user/login endpoint test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import * as request from 'supertest';
+import * as assert from 'assert';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
-import { INestApplication } from '@nestjs/common';
+import {HttpStatus, INestApplication} from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -21,4 +22,27 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it(`/v1/user/login (POST) correct credentials`, () => {
+    return request(app.getHttpServer())
+        .post('/v1/user/login')
+        .send({email: 'admin@admin.ru', password: '12345678'})
+        .expect(HttpStatus.OK)
+        .then(response => {
+          assert(typeof response.body.token === 'string');
+        });
+  });
+
+  it(`/v1/user/login (POST) incorrect credentials`, () => {
+    return request(app.getHttpServer())
+        .post('/v1/user/login')
+        .send({email: 'dmin@admin.ru', password: '12345678'})
+        .expect(HttpStatus.UNAUTHORIZED)
+        .then(response => {
+          assert(JSON.stringify(response.body) === JSON.stringify(
+              { statusCode: 401, error: 'Unauthorized' },
+          ));
+        });
+  });
+
 });
